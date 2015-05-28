4dd05e49019c238ddea9cc9d83a42da5cbdd1dcd
Jose E. Nunez <omitted@omitted.org>
2015-05-28 07:19:02 -0400

    very first version of this code

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # NewRelic_instrumentation_SDK
-example of the NewRelic instrumentation SDK in C
+
+Example of the `NewRelic Instrumentation SDK` in C
+
+# WIP
+
+This project is a *work in progress*. The implementation is *incomplete* and subject to change. The documentation can be inaccurate.
+
+This is the first version of this document.
+
+# How to install
+
+Download the `NewRelic Instrumentation SDK` for C/C++:
+
+    NR_DEST_DIR=~/src/newrelic_agent_sdk_installation/
+    
+    mkdir  -p "${NR_DEST_DIR}"
+    curl  http://download.newrelic.com/agent_sdk/nr_agent_sdk-v0.16.1.0-beta.x86_64.tar.gz \
+          -o  "${NR_DEST_DIR}"/nr_agent_sdk-v0.16.1.0-beta.x86_64.tar.gz
+    
+    cd   "${NR_DEST_DIR}"
+    tar  xzf  nr_agent_sdk-v0.16.1.0-beta.x86_64.tar.gz
+
+Copy the NewRelic SDK's `log4cplus.properties` to standard location (could be left in current directory where the instrumented program runs):
+
+     mkdir  ~/.newrelic/
+     cp   "${NR_DEST_DIR}"/nr_agent_sdk-v0.16.1.0-beta.x86_64/config/log4cplus.properties   ~/.newrelic/
+      
+     # Optionally, increase the debug level in this 'log4cplus.properties'
+     # (debug messages are written by default to files in /tmp/ -besides to stdout)
+     #
+     ##     Substitute in ~/.newrelic/log4cplus.properties:
+     ##                log4cplus.logger.com.newrelic=info, stdout
+     ##     to:
+     ##                log4cplus.logger.com.newrelic=debug, stdout
+
+Compile the test instrumented program (`Makefile` will be due shortly):
+
+     cc  -o  test_embedded_newrelic_instrum  \
+             test_embedded_newrelic_instrum.c  \
+          -I  "${NR_DEST_DIR}"/nr_agent_sdk-v0.16.1.0-beta.x86_64/include/  \
+          -L  "${NR_DEST_DIR}"/nr_agent_sdk-v0.16.1.0-beta.x86_64/lib/  \
+          -l  newrelic-transaction  -l  newrelic-common  -l newrelic-collector-client 
+
+Run the instrumented program:
+
+     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":"${NR_DEST_DIR}"/nr_agent_sdk-v0.16.1.0-beta.x86_64/lib/
+     ./test_embedded_newrelic_instrum
+
+
+Optional:
+
+     rpm -Uvh https://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm
+     yum install newrelic-sysmond
+
+
+# Some scattered notes about the NewRelic SDK in the first version of this document
+
+
+In the New Relic Agent SDK .pdf document (which is inside the tar-ball downloaded in the `How to install` section above):
+
+"In order to maintain communication with New Relic, the Agent SDK requires a thread to be started that harvests data once per minute. This means that your app will need to be able to start a thread, which will run continuously as long as transactions are being executed.
+
+For example, if you are running perl apps through Apache, then each perl script is a short-lived process that exits upon completion of the script. There is no ability to startup a thread that lives beyond the life of the script.
+
+To solve this problem, we designed the SDK so that your agent can run in two different modes: daemon-mode or embedded-mode. In daemon-mode, you'll run the newrelic-collector-client-daemonthat came with the Agent SDK as a stand-alone process. The daemon will collect and send data to New Relic. In embedded-mode your application will start a thread, which will have the same responsibilities as the daemon but will be running within the same process as your transactions.
+
+All functions in the SDK return integers or longs. Any positive number indicates success and any negative number indicates a failure.
+"
+
+
+Apdex T:
+
+Apdex T is the response time threshold used to measure customer satisfaction. The transaction trace threshold is calculated as four times the Apdex T value. The default Apex T value is 0.5 seconds.
+You can change the Apdex T used across all transactions of your application using the environment variable NEWRELIC_APP_SERVER_APDEX_T. If you don't set this environment variable, the Apdex T will default to 0.5 seconds.
+If you want to change the Apdex T for a specific transaction, go to New Relic and make it a Key Transaction. This will give you the option of changing its Apdex T. Once it's been saved, your agent will be notified the next time it's ready to send data up to New Relic.
+
+
+Transaction traces are automatically generated when transactions exceed the transaction trace threshold (4 x ApdexT). ApdexT has a default value of 0.5 seconds, so the default trace threshold for a transaction is 2 seconds.
+The process to create a Transaction Trace is simple:
+* If the transaction duration is longer than the transaction trace threshold, then a trace will be collected.
+* Note: a maximum of two traces will be sent to New Relic per minute.
+
---
+++ test_newrelic_instrum_api.c
@@ -0,0 +1,205 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/limits.h>
+
+#include "newrelic_common.h"
+#include "newrelic_transaction.h"
+#include "newrelic_collector_client.h"
+
+
+int
+generate_lists_of_files(const char* tmp_file, int newrelic_transaction);
+
+
+int
+process_lists_of_files(const char* tmp_file, int newrelic_transaction,
+                       long* max_exec_size_found, char* max_exec_fname);
+
+
+int
+main(int arc, char** argv)
+{
+    newrelic_register_message_handler(newrelic_message_handler);
+
+    newrelic_init("here-goes-the-NewRelic-license-key-string",
+                  "My Application", "C", "4.8");
+
+    int return_code;
+
+    long newr_transxtion_id = newrelic_transaction_begin();
+    if (newr_transxtion_id < 0)
+        fprintf(stderr, "ERROR: newrelic_transaction_begin() returned %d\n",
+                        newr_transxtion_id);
+
+
+    /* Naming transactions is optional.
+     * Use caution when naming transactions. Issues come about when the
+     * granularity of transaction names is too fine, resulting in hundreds if
+     * not thousands of different transactions. If you name transactions
+     * poorly, your application may get blacklisted due to too many metrics
+     * being sent to New Relic.
+     */
+    if (newr_transxtion_id >= 0) {
+         return_code = newrelic_transaction_set_type_other(newr_transxtion_id);
+         if (return_code < 0)
+            fprintf(stderr, "ERROR: newrelic_transaction_set_type_other() "
+                            "returned %d\n", return_code);
+
+         return_code = newrelic_transaction_set_name(newr_transxtion_id,
+                                                    "find_biggest_executable");
+         if (return_code < 0)
+            fprintf(stderr, "ERROR: newrelic_transaction_set_name() "
+                            "returned %d\n", return_code);
+
+         return_code = newrelic_transaction_set_category(newr_transxtion_id,
+                                                         "backend_processing");
+         if (return_code < 0)
+            fprintf(stderr, "ERROR: newrelic_transaction_set_category() "
+                            "returned %d\n", return_code);
+    }
+
+    /*
+     * We can have used:
+     *    find_pipe = popen("/usr/bin/find / -pern -100 ...", "r");
+     *    if (find_pipe) {
+     *       ...
+     *        ret = pclose(find_pipe);
+     *        if (ret < 0) {
+     *                // find failed
+     *        }
+     *
+     * but for this NewRelic example we would like to split the pipe
+     * in two disjoint NewRelic segments, one NR segment finding the
+     * files, the other processing the output
+     */
+
+    char* temp_file = tempnam("/tmp", "myapp_");
+    return_code = generate_lists_of_files(temp_file, newr_transxtion_id);
+    // fprintf(stderr, "generate_lists_of_files() returned %d\n", return_code);
+
+    if (return_code == 0) {
+        long max_fsize;
+        char max_fname[PATH_MAX];
+        process_lists_of_files(temp_file, newr_transxtion_id, &max_fsize, &max_fname);
+    }
+
+    free(temp_file);
+
+    int error_code = newrelic_transaction_end(newr_transxtion_id);
+
+    return 0;
+}
+
+
+int
+generate_lists_of_files(const char* tmp_file, int newrelic_transaction)
+{
+    long newr_segm_external_find = 0;
+    if (newrelic_transaction >= 0) {
+        newr_segm_external_find =
+                newrelic_segment_external_begin(newrelic_transaction,
+                                                NEWRELIC_ROOT_SEGMENT,
+                                                "localhost", "find");
+        if (newr_segm_external_find < 0)
+            fprintf(stderr, "ERROR: newrelic_segment_external_begin() "
+                            "returned %d\n", newr_segm_external_find);
+    }
+
+    char external_command[32*1024];
+    snprintf(external_command, sizeof external_command,
+             "/usr/bin/find  / -type f -perm -100 -printf '%%s %%p\\n' "
+             "> %s 2>/dev/null", tmp_file);
+
+    fprintf(stderr, "Running: %s\n", external_command);
+
+    int ext_cmd_ret = system(external_command);
+
+    if (newrelic_transaction >= 0 && newr_segm_external_find >= 0) {
+        int ret_code =  newrelic_segment_end(newrelic_transaction,
+                                             newr_segm_external_find);
+        if (ret_code < 0)
+            fprintf(stderr, "ERROR: newrelic_segment_end() returned %d\n",
+                    ret_code);
+    }
+
+    /* The `find` command will exit non-zero if it doesn't have permission
+     * to enter into a subdirectory, and similar cases with permissions
+     * -that is why the `find` command above is executed with '2>/dev/null'.
+     * So WEXITSTATUS(ext_cmd_ret), for the `find` case, may return errors
+     * which the user normally ignores.
+     */
+
+    return ( ext_cmd_ret != -1 ) ?  0: -1;
+}
+
+
+
+int
+process_lists_of_files(const char* tmp_file, int newrelic_transaction,
+                       long* max_exec_size_found, char* max_exec_fname)
+{
+    long newr_segm_internal_proc = 0;
+    if (newrelic_transaction >= 0) {
+        newr_segm_internal_proc =
+                newrelic_segment_generic_begin(newrelic_transaction,
+                                              NEWRELIC_ROOT_SEGMENT,
+                                           "processing_list_execs");
+        if (newr_segm_internal_proc < 0)
+            fprintf(stderr, "ERROR: newrelic_segment_external_begin() "
+                            "returned %d\n", newr_segm_internal_proc);
+    }
+
+    long curr_max_fsize = -1;
+    char curr_max_fname[PATH_MAX];
+
+    FILE *f;
+    f = fopen(tmp_file, "r");
+    if (f) {
+        char line[PATH_MAX+32];
+        long fsize;
+        char fname[PATH_MAX];
+        long numb_lines = 0;
+
+        while (fgets(line, sizeof line, f) != NULL) {
+             sscanf (line, "%ld %s", &fsize, fname);
+             if (fsize > curr_max_fsize) {
+                 curr_max_fsize = fsize;
+                 strncpy(curr_max_fname, fname, sizeof curr_max_fname);
+             }
+             numb_lines++;  /* one line per file, as per output of `find` */
+        }
+
+        fclose(f);
+
+        if (newrelic_transaction >= 0) {
+            char total_numb_executables[32];
+            snprintf(total_numb_executables, sizeof total_numb_executables,
+                     "%ld", numb_lines);
+            int ret_code =
+                 newrelic_transaction_add_attribute(newrelic_transaction,
+                                                    "NUMBER_EXECUTABLES",
+                                                    total_numb_executables);
+            if (ret_code < 0)
+                fprintf(stderr, "ERROR: newrelic_transaction_add_attribute() "
+                                "returned %d\n", ret_code);
+        }
+    } else
+        curr_max_fname[0] = '\0';
+
+    *max_exec_size_found = curr_max_fsize;
+    strncpy(max_exec_fname, curr_max_fname, sizeof curr_max_fname);
+
+    if (newrelic_transaction >= 0 && newr_segm_internal_proc >= 0) {
+        int ret_code =  newrelic_segment_end(newrelic_transaction,
+                                             newr_segm_internal_proc);
+        if (ret_code < 0)
+            fprintf(stderr, "ERROR: newrelic_segment_end() returned %d\n",
+                    ret_code);
+    }
+
+    return (curr_max_fname[0] != 0) ? 0: -1 ;
+}
+
+
