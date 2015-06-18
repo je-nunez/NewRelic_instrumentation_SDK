f19125aa01ae2eeaf43b9856bb02b25efeabfb8b
Jose E. Nunez <je.nunez@yahoo.ca>
2015-06-17 23:25:37 -0400

    download_NewRelic_Agent_SDK.sh: some fixes to download the latest New Relic Agent SDK tar ball

--- download_NewRelic_Agent_SDK.sh
+++ download_NewRelic_Agent_SDK.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-BUILD_DIR=$HOME/src/newrelic_agent_sdk_installation/
+BUILD_DIR="$HOME/src/newrelic_agent_sdk_installation/"
 
-mkdir -p $BUILD_DIR
-url=$( curl  http://download.newrelic.com/agent_sdk/ | egrep -i -e '\.tar\.gz' -e '.tgz' | sed 's/^.*href="//; s/".*//' )
+if [[ ! -d "$BUILD_DIR" ]]; then
+    mkdir -p "$BUILD_DIR"
+fi
 
-echo -e "Downloading $url\n"
+url_path=$( curl  http://download.newrelic.com/agent_sdk/ | \
+            egrep -i -e '\.tar\.gz' -e '\.tgz' | \
+            sed 's/^.*href="//; s/".*//' )
 
-curl  "http://download.newrelic.com/agent_sdk/$url"  -o  "$BUILD_DIR/nr_agent_sdk.tar.gz"
+echo -e "Downloading http://download.newrelic.com/agent_sdk/$url_path\n"
 
-echo  -e "\n------ Extracting $BUILD_DIR/nr_agent_sdk.tar.gz ...\n"
+curl  -o  "$BUILD_DIR/nr_agent_sdk.tar.gz" \
+      "http://download.newrelic.com/agent_sdk/$url_path"
 
-cd $BUILD_DIR && \
+curl_exit_code=$?
+if [[ $curl_exit_code -ne 0 ]]; then
+      echo "ERROR: curl exit-code was $curl_exit_code."
+      echo "Please make sure that the file " \
+           "$BUILD_DIR/nr_agent_sdk.tar.gz exists and is valid."
+      echo "Aborting."
+      exit 1
+fi
+
+echo  -e "\nExtracting $BUILD_DIR/nr_agent_sdk.tar.gz ...\n"
+
+cd "$BUILD_DIR" && \
    tar  -xzf  nr_agent_sdk.tar.gz
+
+tar_exit_code=$?
+if [[ $tar_exit_code -ne 0 ]]; then
+      echo "ERROR: tar exit-code was $tar_exit_code."
+      echo "Please make sure that the file " \
+           "$BUILD_DIR/nr_agent_sdk.tar.gz is a tar-gzipped file."
+      echo "Aborting."
+      # exit with a different error code than the above for curl
+      exit 2
+fi
 
 nr_agent_base_directory=$( find $(pwd) -maxdepth 1 -mindepth 1 -type d -cmin -5 )
 
@@ -20,12 +45,25 @@ echo $nr_agent_base_directory
 
 ln  -sf  "$nr_agent_base_directory"   $(pwd)/nr_agent_sdk_base_dir
 
-echo -e "\n------ $BUILD_DIR is ready.\n"
+echo -e "\n$BUILD_DIR is ready.\n"
 
+# Last step
 # Prepare the ~/.newrelic/log4plus.properties files
 
-echo -e "Preparing NewRelic logging settings in $HOME/.newrelic/log4cplus.properties ...\n"
-mkdir  $HOME/.newrelic/
-cp   $BUILD_DIR/nr_agent_sdk-v0.16.2.0-beta.x86_64/config/log4cplus.properties  $HOME/.newrelic/
-sed -i 's#log4cplus.logger.com.newrelic=info,#log4cplus.logger.com.newrelic=debug,#'   $HOME/.newrelic/log4cplus.properties
+if [[ ! -d "$HOME/.newrelic/" ]]; then
+    mkdir  $HOME/.newrelic/
+fi
+if [[ ! -f "$HOME/.newrelic/log4cplus.properties" ]]; then
+    echo -e "Preparing NewRelic logging settings in " \
+            "$HOME/.newrelic/log4cplus.properties ...\n"
+
+    cp "$nr_agent_base_directory/config/log4cplus.properties" $HOME/.newrelic/
+    sed -i 's#log4cplus.logger.com.newrelic=info,#log4cplus.logger.com.newrelic=debug,#' \
+           $HOME/.newrelic/log4cplus.properties
+else
+    echo -e "$HOME/.newrelic/log4cplus.properties exists. " \
+            "Skipping creating it.\n"
+fi
+
+echo -e "Done"
 
